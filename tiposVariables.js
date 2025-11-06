0aa3125c55e21e8c9f007a00223c7e8e8f45be44
MelissaLinanGonzalez <melissa16800@gmail.com>
2025-11-06 20:32:36 +0100

    Iniciacion a JavaScript con los diferentes tipos de variables

---
+++ tiposVariables.js
@@ -0,0 +1,42 @@
+// Reglas de nombres de variables en JavaScript
+
+// letras, $, _, posteriormente se pueden usar números
+let $miNumero1 = 10;
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+console.log(_miNumero2);
+
+let miNumero3 = 30;
+console.log(miNumero3);
+
+// JavaScript es sensible a mayúsculas y minúsculas
+
+
+// Tipos de datos en JS
+let miEntero = 10;
+console.log(miEntero);
+console.log(typeof miEntero);
+
+miEntero = "Ahora soy una cadena";
+console.log(typeof miEntero);
+
+let miFlotante = 10.5;
+console.log(miFlotante);
+console.log(typeof miFlotante);
+
+let miCadena = "hola"; // se podría poner 'hola' o `hola`
+console.log(miCadena);
+console.log(typeof miCadena);
+
+let miBooleano = true;
+console.log(miBooleano);
+console.log(typeof miBooleano);
+
+let miNull = null;
+console.log(miNull);
+console.log(typeof miNull);
+
+let miUndefined = undefined;
+console.log(miUndefined);
+console.log(typeof miUndefined);
