26038fae2f897fabcf2ad3b0c8042243620ba111
MelissaLinanGonzalez <melissa16800@gmail.com>
2025-11-06 20:43:27 +0100

    Constantes

---
+++ TeoriaInicial/constantes.js
@@ -0,0 +1,10 @@
+// Constantes
+const MI_CONSTANTE = 42;
+
+//MI_CONSTANTE = 100; // No podemos cambiar el valor de una constante
+console.log(MI_CONSTANTE);
+
+console.log(Math.PI);
+
+// Constante segundos por minuto
+const SEGUNDOS_POR_MINUTO = 60;
---
+++ TeoriaInicial/hoisting.js
@@ -0,0 +1,7 @@
+// Hoisting, Podemos usar una variable y después declararla
+// Si se usa let, no podemos usarla antes de declararla ni volver a definir la variable, solo funciona con var
+
+var x; // 1. Declarar la variable
+x = 10; // 2. Inicializar la variable
+
+console.log(x);
