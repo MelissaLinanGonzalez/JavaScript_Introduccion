89268ad4c0656044cff968169ee951bc73e901bf
MelissaLinanGonzalez <melissa16800@gmail.com>
2026-02-02 19:37:11 +0100

    Variables y métodos estáticos de la clase

--- Clases/09-01-Clases.js
+++ Clases/09-01-Clases.js
@@ -2,9 +2,24 @@
 // No se aplica el concepto de hoisting
 
 class Persona {
+
+    static contadorPersona = 0; // Atributo de la clase. Se accede con Persona.contadorObjetosPersona
+
+    email = 'valor por defecto email' // Atributo de los objetos. Se accede con objeto.email
+
+    static get MAX_OBJ(){
+        return 5;
+    }
+
     constructor(nombre, apellido){
         this._nombre = nombre; // Convención: usar guion bajo para propiedades privadas
         this._apellido = apellido;
+        if (Persona.contadorPersona < Persona.MAX_OBJ){
+            this.idPersona = ++Persona.contadorPersona;
+        } else{
+            console.log('Se ha superado el máximo de objetos permitidos');
+        }
+        //console.log('Se incrementa el contador: ' + Persona.contadorPersona);
     }
 
     get nombre(){
@@ -30,7 +45,15 @@ class Persona {
     toString(){
         // Se aplica polimorfismo (múltiples formas en tiempo de ejecución)
         // El método que se ejecuta depende si es una referencia de tipo padre o hijo
-        return this.nombreCompleto();
+        return this.idPersona + ' ' +this.nombreCompleto();
+    }
+
+    static saludar(){
+        console.log('Saludos desde el método static');
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre + ' ' + persona.apellido);
     }
 }
 
@@ -58,6 +81,7 @@ class Empleado extends Persona {
 let persona1 = new Persona('Amparo', 'Rodríguez');
 persona1.nombre = 'Amparito'; // se usa el setter
 console.log(persona1.nombre); // se usa el getter
+console.log(persona1); // se usa el getter
 
 let persona2 = new Persona('Pepe', 'Pérez');
 console.log(persona2)
@@ -70,3 +94,24 @@ console.log(empleado1.departamento); // Se usa el getter de la clase hija.
 console.log(empleado1.nombreCompleto());
 
 console.log(empleado1.toString());
+
+// persona1.saludar(); no es posible llamar a un método static desde un objeto
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+
+console.log(Persona.contadorPersona);
+console.log(Empleado.contadorPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+
+console.log(Persona.MAX_OBJ);
+
+let persona3 = new Persona('Jesus', 'Lusan');
+let persona4 = new Persona('Miguel', 'García');
+let persona5 = new Persona('Rosa', 'López');
+console.log(persona5.toString());
